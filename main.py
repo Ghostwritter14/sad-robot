ed5980c37d35da01752f82e3c148d6ec6df78630
soham <sohamrvd@gmail.com>
2023-09-27 23:50:42 +0100

    added a gui interface.

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
+from PyQt6.QtWidgets import QApplication, QVBoxLayout, QWidget, QTextEdit, QLineEdit, QPushButton
 from textblob import TextBlob
+import pyttsx3
+import sys
 
 
 class SadRobot:
     def __init__(self):
         self.happiness = 0
+        self.engine = pyttsx3.init()
+        self.set_voice_and_rate()
+
+    def set_voice_and_rate(self):
+        voices = self.engine.getProperty('voices')
+        voice_index = 1
+        self.engine.setProperty('voice', voices[voice_index].id)
+        rate = self.engine.getProperty('rate')
+        self.engine.setProperty('rate', rate - 50)
 
     def analyze_sentiment(self, text):
         blob = TextBlob(text)
         return blob.sentiment.polarity
+
+    def speak(self, text):
+        self.engine.say(text)
+        self.engine.runAndWait()
 
     def calculate_happiness(self, sentiment):
         if sentiment > 0:
@@ -17,9 +33,11 @@ class SadRobot:
         else:
             return 10
 
-    def increase_happiness(self, sentiment):
-        if sentiment > 0:
-            self.happiness += 1
+    def interact(self, user_input):
+        sentiment = self.analyze_sentiment(user_input)
+        self.happiness += self.calculate_happiness(sentiment)
+        response = self.generate_response(sentiment)
+        return response
 
     def generate_response(self, sentiment):
         if sentiment > 0:
@@ -29,37 +47,43 @@ class SadRobot:
         else:
             return "I feel that way as well."
 
-    def interact(self, user_input):
-        sentiment = self.analyze_sentiment(user_input)
-        self.happiness += self.calculate_happiness(sentiment)
-        response = self.generate_response(sentiment)
-        return response
+
+class SadRobotGUI(QWidget):
+    def __init__(self, sad_robot):
+        super().__init__()
+        self.sad_robot = sad_robot
+        layout = QVBoxLayout()
+        self.output = QTextEdit()
+        self.output.setReadOnly(True)
+        layout.addWidget(self.output)
+        self.input_line = QLineEdit()
+        self.input_line.setPlaceholderText("Type your text here...")
+        layout.addWidget(self.input_line)
+        send_button = QPushButton("Send")
+        send_button.clicked.connect(self.handle_send)
+        layout.addWidget(send_button)
+        self.setLayout(layout)
+
+    def handle_send(self):
+        user_input = self.input_line.text()
+        if user_input:
+            response = self.sad_robot.interact(user_input)
+            self.output.append(f"You: {user_input}")
+            self.output.append(f"Sad Robot: {response}")
+            self.output.append(f"Sad Robot Happiness Level: {self.sad_robot.happiness}")
+            self.sad_robot.speak(f"Sad Robot: {response}")
+            self.input_line.clear()
 
 
 def main():
+    app = QApplication(sys.argv)
     sad_robot = SadRobot()
-    print("Welcome to the Sad Robot Game!")
-    print("Type 'exit' to end the game.")
-    while True:
-        user_input = input("You: ")
-        if user_input.lower() == 'exit':
-            print("Goodbye!")
-            break
-        response = sad_robot.interact(user_input)
-        print(f"Sad Robot: {response}")
-        print(f"Sad Robot Happiness Level: {sad_robot.happiness}")
-        if sad_robot.happiness >= 100:
-            print("Congratulations! You have made the Sad Robot happy!")
-            play_again = input("Do you want to play again? (yes/no): ")
-            if play_again.lower() == 'yes':
-                sad_robot.happiness = 0  # reset the points
-                print("Let's play again!")
-                print("Type 'exit' to end the game.")
-            else:
-                print("Goodbye!")
-                break
+    gui = SadRobotGUI(sad_robot)
+    gui.show()
+    sys.exit(app.exec())
 
 
 if __name__ == "__main__":
     main()
+
 
